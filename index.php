dd648abff799fd181b10b8a25ad7911a2f59ca09
Studio Buque <eddyramos@mail4.studiobuque.com>
2016-08-19 02:04:01 -0500

    probando mailgun

--- index.php
+++ index.php
@@ -16,3 +16,15 @@ if(empty($_POST['FNAME'])      ||
    }
 
 var_dump($_POST);
+
+$mgClient = new Mailgun('key-6457d2a0621240856976be1a081ab670');
+$domain = "sandboxd44d446510644d9ea0440e19424236f3.mailgun.org";
+
+$result = $mgClient->sendMessage($domain, array(
+    'from'    => 'Inscripcion Ucil <inscripción@ucil.sicii.com.mx>',
+    'to'      => 'Eddy Ramos <all.eddyramos@gmail.com>',
+    'subject' => 'Probando salida',
+    'text'    => 'Tu correo hacepto la salida.'
+));
+
+var_dump($result);
