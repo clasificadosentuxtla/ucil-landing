32273fa35034e85b808e368ba74aa45c90ef93a8
LABORATORIO01\laboratorio UCIL 02 <deasarrolloucil@gmail.com>
2016-08-20 10:39:27 -0500

    Probando con mailchimp

--- index.php
+++ index.php
@@ -5,71 +5,55 @@ require_once __DIR__ . '/vendor/autoload.php';
 // use Mailgun\Mailgun;
 use Nette\Mail\Message;
 use Nette\Mail\SmtpMailer;
+use Mailgun\Mailgun;
 
 
 // Check for empty fields
 if(empty($_POST['FNAME'])      ||
-   empty($_POST['EMAIL'])     ||
-   empty($_POST['PHONE'])     ||
-   !filter_var($_POST['EMAIL'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
+     empty($_POST['EMAIL'])     ||
+     empty($_POST['PHONE'])     ||
+     !filter_var($_POST['EMAIL'],FILTER_VALIDATE_EMAIL))
+     {
+     echo "No arguments Provided!";
+     return false;
+     }
 
-var_dump($_POST);
+$data['name'] = $_POST['FNAME'];
+$data['email'] = $_POST['EMAIL'];
+$data['phone'] = $_POST['PHONE'];
+$data['from'] = 'Inscripcion Ucil <inscripciones@ucil.sicii.com.mx>';
+$data['to'] = (string) $_POST['FNAME'].' <'.$data['email'].'>';
 
-echo "<h3>Crear objeto</h3>";
+var_dump($data);
+
+echo "<h2>Nette</h2>";
 $mail = new Message;
-
-var_dump($mail);
-
-echo "<h3>Asignar los correos</h3>";
-$mail->setFrom('Inscripcion Ucil <inscripciones@ucil.sicii.com.mx>')
-    ->addTo('Eddy Ramos <all.eddyramos@gmail.com>')
-    ->addTo('lievanoabadiaj@gmail.com')
-    ->setSubject('Probando salida')
-    ->setBody("Hello, Your order has been accepted.");
-
-var_dump($mail);
-
-echo "<h3>Configurar SMTP</h3>";
+$mail->setFrom($data['from'])
+        ->addTo($data['to'])
+        ->setSubject('Probando salida')
+        ->setBody("Tu correo acepto la salida.");
 $mailer = new SmtpMailer([
-        'host' => 's55.grupocopydata.com',
-        'username' => 'inscripciones@ucil.sicii.com.mx',
-        'password' => 'sb$SC%10',
-        'secure' => 'ssl'
+                'host' => 's55.grupocopydata.com',
+                'username' => 'inscripciones@ucil.sicii.com.mx',
+                'password' => 'sb$SC%10',
+                'secure' => 'ssl'
 ]);
-/*
-,
-        'context' =>  [
-            'ssl' => [
-                'capath' => '/path/to/my/trusted/ca/folder',
-             ],
-        ],
-*/
-
-var_dump($mailer);
-
-
-echo "<h3>Enviar</h3>";
 $result = $mailer->send($mail);
 
-var_dump($result);
+var_dump($mail, $mailer, $result);
 
-/*
-echo "<h3>Crear objeto</h3>";
-$mgClient = new Mailgun('key-6457d2a0621240856976be1a081ab670');
-$domain = "sandboxd44d446510644d9ea0440e19424236f3.mailgun.org";
-var_dump($mgClient);
+echo "<h2>Mailgun</h2>";
 
-echo "<h3>Probar envio</h3>";
-$result = $mgClient->sendMessage($domain, array(
-    'from'    => 'Inscripcion Ucil <inscripción@ucil.sicii.com.mx>',
-    'to'      => 'Eddy Ramos <all.eddyramos@gmail.com>',
-    'subject' => 'Probando salida',
+$mg = new Mailgun("key-6457d2a0621240856976be1a081ab670");
+$domain = "sandboxd44d446510644d9ea0440e19424236f3.mailgun.or";
+$result_mg = $mg->sendMessage($domain, array(
+    'from'    => $data['from'], 
+    'to'      => $data['to'], 
+    'subject' => 'Probando salida', 
     'text'    => 'Tu correo acepto la salida.'
 ));
 
-var_dump($result);
-*/
+var_dump($mg, $result_mg);
+
+echo "Redireccionar a: http://ucil.mx.57aa4811dee41.635775913472199676-1325727676.mini1.studiobuque.com/landing/57aa4811dee41/gracias.php";
+
